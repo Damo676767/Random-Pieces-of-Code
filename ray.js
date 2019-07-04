41a2761d10ce985bf14a3822bed0453b9e2a7b90
Damo676767 <52542879+Damo676767@users.noreply.github.com>
2019-07-05 02:30:43 +1000

    Add files via upload

---
+++ ray.js
@@ -0,0 +1,57 @@
+// Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/145-2d-ray-casting.html
+// https://youtu.be/TOEi6T2mtHo
+
+// 2D Ray Casting
+// https://editor.p5js.org/codingtrain/sketches/Nqsq3DFv-
+
+// Slight optimisation to cast done by Damien Porter to speed up
+// and better reflect how the calculation is actually performed
+
+class Ray {
+  constructor(pos, angle) {
+    this.pos = pos;
+    this.dir = p5.Vector.fromAngle(angle);
+  }
+
+  lookAt(x, y) {
+    this.dir.x = x - this.pos.x;
+    this.dir.y = y - this.pos.y;
+    this.dir.normalize();
+  }
+
+  show() {
+    stroke(255);
+    push();
+    translate(this.pos.x, this.pos.y);
+    line(0, 0, this.dir.x * 10, this.dir.y * 10);
+    pop();
+  }
+
+  cast(wall) {
+    const x1 = wall.a.x;
+    const y1 = wall.a.y;
+    const x2 = wall.b.x;
+    const y2 = wall.b.y;
+
+    const den = (y1 - y2) * this.dir.x - (x1 - x2) * this.dir.y;
+    if (den == 0) {
+      return;
+    }
+      
+    const x3 = this.pos.x;
+    const y3 = this.pos.y
+
+    const t = ((y1 - y3) * this.dir.x - (x1 - x3) * this.dir.y ) / den;
+    const u = -((x1 - x2) * (y1 - y3) - (y1 - y2) * (x1 - x3)) / den;
+    if (t > 0 && t < 1 && u > 0) {
+      const pt = createVector();
+      pt.x = x1 + t * (x2 - x1);
+      pt.y = y1 + t * (y2 - y1);
+      return pt;
+    } else {
+      return;
+    }
+  }
+}
+
